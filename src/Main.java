3ec3e794b0e02e57bcd2c5c44d2a43e3e7bb3aa5
eran <eron.www.eron@gmail.com>
2024-04-10 01:03:21 +0600

    test

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] array = {"a1 ","a2 ","a3 "};
+        for (String i : array){
+            System.out.println("I love you " + i);
+        }
+
+    }
+
+//    static void metod(int... varargs) {
+//        int sum = 0;
+//
+//
+//        for (int i = 0; i < varargs.length; i++) {
+//            sum = sum + varargs[i];
+//        }
+//        System.out.println(sum);
+//
+//    }
+}
